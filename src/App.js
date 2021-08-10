088afb10ba815b846ce45d053beed07e74c52a2c
Wallison Martins <wallisonpinheiro1@gmail.com>
2021-08-10 20:30:30 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
-import Home from './pages/Home';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Header />
+      <Routes />
+      <Footer />
       <GlobalStyle />
     </BrowserRouter>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import Header from '../../components/Header';
 import Card from '../../components/Card';
-import Footer from '../../components/Footer';
 
 import { Container, ProductList } from './styles';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Container>
         <h1>Produtos em destaque</h1>
         <ProductList>
           <Card />
         </ProductList>
       </Container>
-      <Footer />
     </>
   );
 }
